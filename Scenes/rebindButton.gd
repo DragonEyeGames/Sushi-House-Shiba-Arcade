d1e94389c191610dddc25735d304fbb7d3840165
Taxalottle Syndicate <bthole41@gmail.com>
2025-10-07 11:42:37 -0400

    Key rebind
    You can now rebind you keys!

---
+++ Scenes/rebindButton.gd
@@ -0,0 +1,42 @@
+extends Control
+
+@export var textToShow :=""
+@export var actionToRebind := ""
+var waitingForInput := false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	$RichTextLabel.text=textToShow
+	if(not waitingForInput):
+		$Button.text=InputMap.action_get_events(actionToRebind)[0].as_text()
+	else:
+		$Button.text="Waiting for input..."
+
+func start_rebind():
+	waitingForInput = true
+	print("Press a key to rebind", actionToRebind)
+
+func _input(event):
+	if(not waitingForInput):
+		return
+	if waitingForInput and event is InputEventKey and event.pressed:
+		accept_event()
+		rebind_action(actionToRebind, event)
+		waitingForInput = false
+	elif event is InputEventMouseButton and event.pressed:
+		if event.double_click:
+			return 
+		accept_event()
+		rebind_action(actionToRebind, event)
+		waitingForInput = false
+
+func rebind_action(action_name: String, event):
+	InputMap.action_erase_events(action_name)
+	InputMap.action_add_event(action_name, event)
+	print("Rebound", action_name, "to", event.as_text())
+	waitingForInput=false
--- Scripts/start_scene.gd
+++ Scripts/start_scene.gd
@@ -8,9 +8,9 @@ func _ready() -> void:
 		GameManager.musicValue=save.musicVolume
 		GameManager.masterValue=save.masterVolume
 		GameManager.sfxValue=save.sfxVolume
-	$CanvasLayer/Control/HSlider.value=GameManager.masterValue
-	$CanvasLayer/Control/HSlider2.value=GameManager.sfxValue
-	$CanvasLayer/Control/HSlider3.value=GameManager.musicValue
+	$Audio/Control/HSlider.value=GameManager.masterValue
+	$Audio/Control/HSlider2.value=GameManager.sfxValue
+	$Audio/Control/HSlider3.value=GameManager.musicValue
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,7 +29,7 @@ func _hover_exited() -> void:
 
 
 func _pressed() -> void:
-	$CanvasLayer/AnimationPlayer.play("showUp")
+	$SettingsMenu/AnimationPlayer.play("showUp")
 
 
 func _play_hovered() -> void:
@@ -43,9 +43,9 @@ func _on_texture_button_mouse_exited() -> void:
 
 
 func _on_texture_button_pressed() -> void:
-	GameManager.masterValue=$CanvasLayer/Control/HSlider.value
-	GameManager.sfxValue=$CanvasLayer/Control/HSlider2.value
-	GameManager.musicValue=$CanvasLayer/Control/HSlider3.value
+	GameManager.masterValue=$Audio/Control/HSlider.value
+	GameManager.sfxValue=$Audio/Control/HSlider2.value
+	GameManager.musicValue=$Audio/Control/HSlider3.value
 	var save = SaveData.new()
 	save.masterVolume=GameManager.masterValue
 	save.sfxVolume=GameManager.sfxValue
@@ -55,7 +55,7 @@ func _on_texture_button_pressed() -> void:
 
 
 func _x() -> void:
-	$CanvasLayer/AnimationPlayer.play("hide")
+	$SettingsMenu/AnimationPlayer.play("hide")
 
 
 func _master_changed(value: float) -> void:
@@ -77,3 +77,43 @@ func _music(value: float) -> void:
 	if(value==-80):
 		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), -80)
 	GameManager.musicValue=value
+
+
+func _on_audio_pressed() -> void:
+	$SettingsMenu/AnimationPlayer.play("hide")
+	$Audio/AnimationPlayer.play("showUp")
+
+
+func _on_audio_mouse_entered() -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property($SettingsMenu/Control/Audio, "scale", Vector2(.2, .2), .25)
+
+
+func _on_audio_mouse_exited() -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property($SettingsMenu/Control/Audio, "scale", Vector2(.18, .18), .25)
+
+
+func _audio_x_pressed() -> void:
+	$SettingsMenu/AnimationPlayer.play("showUp")
+	$Audio/AnimationPlayer.play("hide")
+
+
+func _on_input_mouse_entered() -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property($SettingsMenu/Control/Input, "scale", Vector2(.2, .2), .25)
+
+
+func _on_input_mouse_exited() -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property($SettingsMenu/Control/Input, "scale", Vector2(.18, .18), .25)
+
+
+func _on_settings_input_pressed() -> void:
+	$SettingsMenu/AnimationPlayer.play("hide")
+	$KeyRebind/AnimationPlayer.play("showUp")
+
+
+func _input_remap_x_() -> void:
+	$SettingsMenu/AnimationPlayer.play("showUp")
+	$KeyRebind/AnimationPlayer.play("hide")
